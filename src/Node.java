23ee289b95496348bfe2f592cfe2f30f48002288
Fernando Zarate <fernando.zarate@mi.unc.edu.ar>
2026-05-01 20:24:38 -0300

    etapa 2 primera implementacion

--- src/Node.java
+++ src/Node.java
@@ -3,13 +3,33 @@ package src;
 public class Node {
 
     // Atributtes
-    Integer id;
-    String status; // "Free", "Busy", "Out of Service"
-    Integer jobsCounter; // Number of jobs executed by this node.
+    private Integer id;
+    private String status; // "Free", "Busy", "Out of Service"
+    private Integer jobsCounter; // Number of jobs executed by this node.
 
     public Node(Integer id) {
         this.id = id;
         this.status = "Free";
         this.jobsCounter = 0;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getJobsCounter() {
+        return jobsCounter;
+    }
+
+    public void incrementJobsCounter() {
+        this.jobsCounter++;
     }
 }
--- src/PreExecutionCheck.java
+++ src/PreExecutionCheck.java
@@ -1,13 +1,75 @@
 package src;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class PreExecutionCheck implements Runnable {
+
+    ArrayList<Job> JobsQueue;
+    ArrayList<Job> jobsExecution;
+    ArrayList<Job> jobsFailed;
+    Node[] nodes;
+
+    Random random;
     
-    public PreExecutionCheck() {
+    public PreExecutionCheck(ArrayList<Job> JobsQueue, ArrayList<Job> JobsExcecution, ArrayList<Job> JobsFailed, Node[] nodes) {
         
+        this.JobsQueue = new ArrayList<>();
+        this.jobsExecution = new ArrayList<>();
+        this.jobsFailed = new ArrayList<>();
+        this.nodes = new Node[200];
+
+        this.JobsQueue = JobsQueue;
+        this.jobsExecution = JobsExcecution;
+        this.jobsFailed = JobsFailed;
+        this.nodes = nodes;
+        random = new Random();
     }
 
     @Override
-    public void run() {
-        
+    public void run() 
+    {
+
+        while(true)
+        {
+
+            if(JobsQueue.size() == 0)
+            {
+                continue;
+            }
+            
+            // tomamos un job aleatorio de los Jobs en cola
+            int i_random_job = Politic.randomIndex(JobsQueue.size());
+            Job job_taken = JobsQueue.remove(i_random_job);
+
+            if(Politic.executionSuccess())
+            {
+
+                // vamos a recorrer el arreglo buscando un nodo ocupado
+                for(int i=0; i<nodes.length; i++)
+                {
+                    if( nodes[i].getStatus().equals("Busy"))
+                    {
+                        nodes[i].setStatus("Free");
+                        nodes[i].incrementJobsCounter();
+                        jobsExecution.add(job_taken);
+                        break;
+                    }
+                }
+
+            }
+            else
+            {
+                for(int i=0; i<nodes.length; i++)
+                {
+                    if( nodes[i].getStatus().equals("Busy"))
+                    {
+                        nodes[i].setStatus("Out of Service");
+                        nodes[i].incrementJobsCounter();
+                        jobsFailed.add(job_taken);
+                        break;
+                    }
+                }
+            }
+        }    
     }
 }
