0985eadbdab57d6df2baea6c0ddd35dada3b8634
miglierinicarlos <miglierinicarlos@gmail.com>
2026-05-01 18:48:44 -0300

    feat: add Politic class with probabilistic decisions

--- src/Politic.java
+++ src/Politic.java
@@ -1,5 +1,24 @@
 package src;
 
+import java.util.Random;
+
 public class Politic {
-    
+
+    private static final Random random = new Random();
+
+    public static boolean isValidJob() {
+        return random.nextInt(100) < 85; // 85% válido
+    }
+
+    public static boolean executionSuccess() {
+        return random.nextInt(100) < 90; // 90% éxito
+    }
+
+    public static boolean finalCheckCorrect() {
+        return random.nextInt(100) < 95; // 95% correcto
+    }
+
+    public static int randomIndex(int size) {
+        return random.nextInt(size);
+    }
 }
