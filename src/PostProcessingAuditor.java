c612f772665a86d02d36e0a963e6f06e23e6314d
Ignacio Giraudo <ignacio.jose.giraudo@mi.unc.edu.ar>
2026-05-02 16:55:20 -0300

    Ahi hice el post processing auditor, despues habria que modificar el tiempo del sleep para estimar la duracion del programa a 15-30 segundos. El tiempo del else es para que no esté constantemente chequeando hasta que aparezca un job

--- src/PostProcessingAuditor.java
+++ src/PostProcessingAuditor.java
@@ -1,13 +1,56 @@
 package src;
 
+import java.util.ArrayList;
+
 public class PostProcessingAuditor implements Runnable {
 
-    public PostProcessingAuditor() {
+    private ArrayList<Job> jobsFinished;
+    private ArrayList<Job> jobsValidated;
+    private ArrayList<Job> jobsFailed;
 
+    public PostProcessingAuditor(ArrayList<Job> jobsFinished, ArrayList<Job> jobsValidated,
+                                 ArrayList<Job> jobsFailed) {
+        this.jobsFinished = jobsFinished;
+        this.jobsValidated = jobsValidated;
+        this.jobsFailed = jobsFailed;
     }
 
     @Override
     public void run() {
-
+        while (true) {
+            Job job = null;
+            synchronized (jobsFinished) {
+                if (!jobsFinished.isEmpty()) {
+                    job = jobsFinished.remove(0);
+                }
+            }
+            if (job != null) {
+                // Acá tenemos que simular un tiempo pero habria que ver cuanto por etapa
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+                // Aplica política
+                if (Politic.finalCheckCorrect()) {
+                    job.stage = 4;
+                    synchronized (jobsValidated) {
+                        jobsValidated.add(job);
+                    }
+                } else {
+                    synchronized (jobsFailed) {
+                        jobsFailed.add(job);
+                    }
+                }
+            }
+            else {
+                try  {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
